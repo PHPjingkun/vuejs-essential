fceea7a3465645f98349a8671471521820ecf7c9
1078156216@qq.com <>
2022-09-02 18:23:47 +0800

    3.2

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="navbar navbar-default topnav">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">
+                    <img :src="logo.src" :alt="logo.title">
+                    <span class="title">{{ logo.title }}</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheHeader',
+        data() {
+            return {
+                logo: {
+                    src: `${this.uploadsUrl}communities/hIZjRRdF8oVYZy69XJnT.png!/both/44x44`,
+                    title: 'Learnku Vue.js'
+                }
+            }
+        },
+        beforeCreate() {
+            this.uploadsUrl = 'https://cdn.learnku.com//uploads/'
+        }
+    }
+</script>
+
+<style scoped>
+    .title { color: black;}
+    .navbar-brand > img { display: inline-block; }
+    .navbar-default .navbar-nav > .active > a { background: rgba(0,0,0,.03);}
+</style>
